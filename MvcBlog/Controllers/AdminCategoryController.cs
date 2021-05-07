dc76b4f813d9e977f396f5c8358a8caef4a903fb
serhat potur <serhat_potur@hotmail.com>
2021-05-07 22:50:02 +0300

    Admin teması eklendi

---
+++ MvcBlog/Controllers/AdminCategoryController.cs
@@ -0,0 +1,49 @@
+﻿using Business.Concrate;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.EntityFramework;
+using Entity.Concrate;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcBlog.Controllers
+{
+    public class AdminCategoryController : Controller
+    {
+        CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        // GET: AdminCategory
+        public ActionResult CategoryList()
+        {
+            var result = categoryManager.GetList();
+            return View(result);
+        }
+        [HttpGet]
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddCategory(Category category)
+        {
+            CategoryValidator categoryValidator = new CategoryValidator();
+            ValidationResult results = categoryValidator.Validate(category);
+            if (results.IsValid)
+            {
+                categoryManager.Add(category);
+                return RedirectToAction("CategoryList");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+                return View();
+            }
+
+        }
+    }
+}
